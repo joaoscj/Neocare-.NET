30306cc51a3fe68d47fa59940d3caf8bd6fd1622
joaoscj <joaodigitalscj@hotmail.com>
2025-10-12 21:38:14 -0300

    Ajustes finais

--- Neocare/Domain/Entities/StressEntry.cs
+++ Neocare/Domain/Entities/StressEntry.cs
@@ -3,7 +3,7 @@ namespace Neocare.Domain.Entities
     public class StressEntry
     {
         public Guid Id { get; set; }
-        public int StressLevel { get; set; } // Scale from 1-10
+        public int StressLevel { get; set; }
         public string Description { get; set; } = string.Empty;
         public List<string> Symptoms { get; set; } = new();
         public DateTime RecordedAt { get; set; }
